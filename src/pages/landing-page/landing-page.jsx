4559f053e6196de378dd9c8bcef47d630b7bc89c
BurakTetikk <burktetk@gmail.com>
2025-07-31 01:45:57 +0300

    created hero component

--- src/pages/landing-page/landing-page.jsx
+++ src/pages/landing-page/landing-page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/header";
+import Hero from "./components/hero";
 
 const LandingPage = () => {
   return (
     <div className="min-h-screen">
       <Header />
+      <Hero />
     </div>
   );
 };
